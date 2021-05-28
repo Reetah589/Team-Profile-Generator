8ee4f48298ac5582005fb2fa2007e0dae938074c
reetah589 <reetah589@gmail.com>
2021-05-28 14:53:30 -0500

    Array Questions added to app

--- app.js
+++ app.js
@@ -1,2 +1,41 @@
+const Employee = [];
 //const manager = equire
+
 //set up an array of questions 
+var questions = [
+{
+    name: "employee name",
+    message: "Please enter the team member's name",
+    type: "input"
+},
+{
+    name: "lists",
+    message: "Please enter the team member's position",
+    type: "input"
+},
+{
+    name: "email",
+    message: "Please enter the team member's email",
+    type: "input"
+},
+{
+    name: "id",
+    message: "Please enter the team member's id number",
+    type: "input"
+},
+{
+    name: "intern",
+    message: "If Intern, please enter the school",
+    type: "input"
+},
+{
+    name: "manager",
+    message: "If Manager, please enter the office number",
+    type: "input"
+},
+{
+    name: "engineer",
+    message: "If Engineer, please enter the GitHub username",
+    type: "input"
+},    
+]
