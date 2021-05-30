9328b13977724ba5a5fd0a5cff9e33fef5e7f142
reetah589 <reetah589@gmail.com>
2021-05-30 10:38:25 -0500

    First tryout failed

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const Employee = [];
-//const manager = equire
+var inquirer = require('inquirer')
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+
+const fs = require("fs")
 
 //set up an array of questions 
 var questions = [
@@ -39,3 +44,19 @@ var questions = [
     type: "input"
 },    
 ]
+
+function init (){
+    inquirer
+    .prompt(questions)
+    .then(data => {
+      console.log(data)
+      console.log(data.title)
+      console.log(data.description)
+      console.log(data.installation)
+      console.log(data.usage)
+      console.log(data.contributing)
+      console.log(data.tests)
+})
+
+init();
+
